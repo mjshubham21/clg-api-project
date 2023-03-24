10f46c2911fd4728d79f7fa195d24992a2b2f485
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-03-24 19:24:57 +0530

    added GET and POST routes.

--- api/server.js
+++ api/server.js
@@ -14,4 +14,19 @@ mongoose
   .then(() => console.log("Connected to DB."))
   .catch(console.error);
 
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    tect: req.body.text,
+  });
+  res.save();
+  res.json(todo);
+});
+
 app.listen(3001, () => console.log("Server started on port 3001."));
