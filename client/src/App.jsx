ffaef65fe0030102794a804bf0ba3a561bbae779
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-03-26 18:14:12 +0530

    addTodo function added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,6 +40,21 @@ function App() {
     setTodos((todos) => todos.filter(todo._id !== data._id));
   };
 
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  };
+
   return (
     <>
       <div className="app">
@@ -62,7 +77,30 @@ function App() {
             </div>
           ))}
         </div>
-        <div className="addPopup" onClick={setPopupActive(true)}>+</div>
+        <div className="addPopup" onClick={setPopupActive(true)}>
+          +
+        </div>
+        {popupActive ? (
+          <div className="popup">
+            <div className="closePopup" onClick={() => setPopupActive(false)}>
+              X
+            </div>
+            <div className="content">
+              <h3> Add todo...</h3>
+              <input
+                type="text"
+                className="add-todo-input"
+                onChange={(e) => setNewTodo(e.target.value)}
+                value={newTodo}
+              />
+              <div className="button" onClick={addTodo}>
+                Create Task...
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
