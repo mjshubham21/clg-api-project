3922c05894fee58c0d26cd66814ce709fff845b2
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-04-18 18:08:44 +0530

    Added weather component from previous project and npm i axios

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,5 +1,59 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function Weather() {
-  return <>HEY.</>;
+  const apiKey = process.env.API_KEY;
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+  const getData = (city) => {
+    if (!city) return;
+    const apiURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=" +
+      apiKey;
+    axios
+      .get(apiURL)
+      .then((res) => {
+        console.log("Response: ", res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  const handleClick = () => {
+    getData(inputCity);
+  };
+
+  const handleChange = (e) => {
+    setInputCity(e.target.value);
+    // e.preventDefault();
+  };
+  useEffect(() => {
+    getData(inputCity);
+  }, []);
+  return (
+    <>
+      <h1> Weather Api.</h1>
+      <div className="container">
+        <input
+          value={inputCity}
+          onChange={handleChange}
+          className="city"
+          type="text"
+        />
+        <button onClick={handleClick} className="search">
+          Search
+        </button>
+      </div>
+      <div className="res">
+        <h4 className="resCity">{data?.name}</h4>
+        <h5 className="temp">{data?.main?.temp} °C</h5>
+      </div>
+    </>
+  );
 }
 
 export default Weather;
