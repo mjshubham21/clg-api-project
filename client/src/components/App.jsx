392e241c46afbaac4ca6a022b02fdd3979987670
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-04-28 20:30:58 +0530

    Removed the extra stuff, no time right now...

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToDo from "./ToDo";
-import Nav from "./Nav";
 import Weather from "./Weather";
-import About from "./About";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
   return (
     <>
-      <Nav />
       <ToDo />
       <Weather />
-      <About />
     </>
   );
 }
