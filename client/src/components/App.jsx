17e6cc780b3690b7d733f09c8a27e250e1067611
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-04-28 11:00:29 +0530

    Adding Nav and Aout component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ToDo from "./ToDo";
+import Nav from "./Nav";
 import Weather from "./Weather";
+import About from "./About";
 
 function App() {
   return (
     <>
+      <Nav />
       <ToDo />
       <Weather />
+      <About />
     </>
   );
 }
