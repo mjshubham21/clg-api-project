60590a25adffb726a0f0ca6437b492d7f6127e78
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-05-13 20:02:03 +0530

    Undid till before signup function.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,32 +1,3 @@
-// import React, { useEffect } from "react";
-// import ToDo from "./ToDo";
-// import Weather from "./Weather";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-// import About from "./About";
-// import Navbar from "./Navbar";
-// import Welcome from "./Welcome";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// function App() {
-//   useEffect(() => {
-//     AOS.init({
-//       duration: 2000,
-//     });
-//   }, []);
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Welcome />
-//       <Routes>
-//         <Route path="/" element={<ToDo />} />
-//         <Route path="/weather" element={<Weather />} />
-//         <Route path="/about" element={<About />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-// export default App;
 //  chatgpt code:
 // import React, { useState, useEffect } from "react";
 // import {
